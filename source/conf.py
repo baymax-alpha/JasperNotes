69f02953dab221d27251817ce837f5821e848262
Jasper He <jasper@JasperdeMacBook-Air.local>
2020-07-06 18:38:27 +0800

    update theme to sphinx-rigado-theme final.

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,7 @@ release = '1'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '3.1'
+# needs_sphinx = '2.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
