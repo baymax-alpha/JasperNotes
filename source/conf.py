daf638b625fcedd0223afba92abe3cb96faa54ca
Jasper He <jasper@JasperdeMacBook-Air.local>
2020-07-06 18:31:11 +0800

    update theme to sphinx-rigado-theme final

--- source/conf.py
+++ source/conf.py
@@ -21,12 +21,23 @@ project = 'JasperNotes'
 copyright = '2020, Jasper He'
 author = 'Jasper He'
 
+#++ The short X.Y version
+version = ''
+#++ The full version, including alpha/beta/rc tags
+release = '1'
+
 # -- General configuration ---------------------------------------------------
+
+# If your documentation needs a minimal Sphinx version, state it here.
+#
+needs_sphinx = '3.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+     'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -35,7 +46,8 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -50,13 +62,18 @@ os.system("pip install sphinx_catalystcloud_theme")
 html_theme = 'sphinx_catalystcloud_theme'
 import sphinx_catalystcloud_theme
 html_theme_path = [sphinx_catalystcloud_theme.get_html_theme_path()]
+
 master_doc = 'index'
+
+#++ The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 #++>
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#--html_static_path = ['_static']
+
+html_static_path = ['_static']
 
 #++< spport markdown using recommonmark
 from recommonmark.parser import CommonMarkParser
